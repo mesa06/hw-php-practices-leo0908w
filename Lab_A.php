2f9c56589a833cbe736ff3ac4ad792f53de6def6
Will Lan <leo0908w@yahoo.com.tw>
2016-10-25 01:22:35 +0000

    籃國維

---
+++ Lab_A.php
@@ -0,0 +1,14 @@
+<?php 
+$star = "";
+
+for ($i = 1; $i < 6; $i++) {
+    $space .= "&nbsp";
+    for ($j = 1; $j < 6; $j++) {
+        $star .= "*";
+        echo "$star <br>";
+    }
+    
+} 
+
+
+?>
---
+++ Lab_F.php
@@ -0,0 +1,14 @@
+<?php
+
+$height = 100;
+
+for ($i = 1; $i <= 5; $i++) {
+    $height = $height / 2;
+    echo "$i : $height<br>";
+
+    
+    
+}
+
+
+?>
